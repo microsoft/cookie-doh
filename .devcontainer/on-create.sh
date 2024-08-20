1de7da3950b8d5530ee1058ca6ba5911dcfb9e8f
Javier Zazo <javierzazo@microsoft.com>
2024-08-20 11:29:00 +0000

    ✨ feat: rootless docker in devcontainer

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-echo "CONFIGURING ROOTLESS DOCKER"
-sudo chown root:docker /var/run/docker.sock
-
 echo "CONFIGURING DIRENV..."
 echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
 echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
@@ -13,12 +10,6 @@ pipx ensurepath
 echo "UPDATE PIP"
 pip3 install -U pip
 
-echo "INSTALLING COMMITIZEN"
-pipx install commitizen
-pipx inject commitizen cz-conventional-gitmoji
-echo 'eval "$(register-python-argcomplete3 cz)"' >> ~/.bashrc
-echo 'eval "$(register-python-argcomplete3 cz)"' >> ~/.zshrc
-
 echo "POETRY INSTALL"
 pipx install poetry
 poetry completions bash >> ~/.bash_completion
@@ -29,11 +20,3 @@ poetry config virtualenvs.in-project true
 direnv allow .
 poetry env remove --all
 poetry install
-
-echo "CONFIGURING PRE-COMMIT"
-if [ ! -d .git ]; then
-    echo "Initializing Git..."
-    git init
-fi
-pipx install pre-commit
-pre-commit install --install-hooks
